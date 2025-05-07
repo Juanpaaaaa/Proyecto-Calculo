79b066f3c035f2811f070ad6731b8e74ad670c8f
Juanpaaaaa <Juanpaorellana866@gmail.com>
2025-05-07 16:20:31 -0600

    Agregado el espacio para los metodos

---
+++ src/proyectocalculo/Metodos.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectocalculo;
+
+/**
+ *
+ * @author juanpablo
+ */
+public class Metodos {
+    
+    public void TeoremaValorMedio(){
+        System.out.println("Teorema valor medio");
+    
+    }
+    
+    public void  TeoremaRolle(){
+        System.out.println("teorema rolle");
+    }
+    
+    public void MetodoNewton(){
+        System.out.println("Metodo newton");
+    
+    }
+    
+    public void DerivadaMaxyMin(){
+        System.out.println("Maximos y minimos");
+    
+    
+   }
+    
+}
--- src/proyectocalculo/ProyectoCalculo.java
+++ src/proyectocalculo/ProyectoCalculo.java
@@ -12,48 +12,43 @@ import java.util.Scanner;
  */
 public class ProyectoCalculo {
 
-    /**
-     * @param args the command line arguments
-     */
+   
+
+
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
-            int opcion;
+        Metodos metodos = new Metodos(); 
+        int opcion;
 
-            do {
-                System.out.println("\n====== PROYECTO DE PRECÁLCULO ======");
-                System.out.println("1. Teorema de Rolle");
-                System.out.println("2. Teorema del Valor Medio");
-                System.out.println("3. Método de Newton");
-                System.out.println("4. Derivadas para Máximos y Mínimos");
-                System.out.println("5. Salir");
-                System.out.print("Seleccione una opción: ");
-                opcion = scanner.nextInt();
+        do {
+            System.out.println("\n====== PROYECTO DE PRECÁLCULO ======");
+            System.out.println("1. Teorema del Valor Medio");
+            System.out.println("2. Teorema de Rolle");
+            System.out.println("3. Método de Newton");
+            System.out.println("4. Derivadas para Máximos y Mínimos");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = scanner.nextInt();
 
-                switch (opcion) {
-                    case 1:
-                        System.out.println(">> Aquí irá el código del Teorema de Rolle");
-                        break;
-                    case 2:
-                        System.out.println(">> Aquí irá el código del Teorema del Valor Medio");
-                        break;
-                    case 3:
-                        System.out.println(">> Aquí irá el código del Método de Newton");
-                        break;
-                    case 4:
-                        System.out.println(">> Aquí irá el código para máximos y mínimos");
-                        break;
-                    case 5:
-                        System.out.println("Gracias por usar el programa.");
-                        break;
-                    default:
-                        System.out.println("Opción inválida.");
-                }
+            switch (opcion) {
+                case 1 -> metodos.TeoremaValorMedio();
+                
+                case 2 -> metodos.TeoremaRolle();
+                
+                case 3 -> metodos.MetodoNewton();
+                
+                case 4 -> metodos.DerivadaMaxyMin();
+                
+                case 5 -> System.out.println("Gracias por usar el programa.");
+                
+                default -> System.out.println("Opción inválida.");
+            }
 
-            } while (opcion != 5);
-        }
+        } while (opcion != 5);
     }
-        // TODO code application logic here
+}    
+     
   
     
 
